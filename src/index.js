0242149b8864fc9ca27b353bcd782e055dc3bc7c
Jefid <99925114+Jefid@users.noreply.github.com>
2022-07-17 21:49:34 -0400

    adding npm gh-pages function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+var ghpages = require("gh-pages");
+
+ghpages.publish("dist", function (err) {});
 
 ReactDOM.render(
   <BrowserRouter>
